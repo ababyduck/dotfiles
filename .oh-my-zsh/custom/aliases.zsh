fa85994417215008e74d5247143f98f1be527cb5
Andrew Kassis <kassisaf@gmail.com>
2018-04-12 22:45:10 -0700

    Cleaned up whitespace

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
-#Basic navigation
+# Basic navigation
 alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../../'
@@ -6,7 +6,7 @@ alias ls='ls -Gp --color=auto'
 alias c='clear'
 alias h='history'
 
-#Safeguards
+# Safeguards
 alias chmod='chmod -v --preserve-root'
 alias chown='chown -v --preserve-root'
 alias chgrp='chgrp -v --preserve-root'
@@ -14,15 +14,16 @@ alias cp='cp -v'
 alias mv='mv -v'
 alias rm='rm -Iv'
 
-#Package Management
-alias pacpurge='sudo pacman -Rns $(pacman -Qtdq)'	# Remove orphaned packages
+# Package Management
+alias pacpurge='sudo pacman -Rns $(pacman -Qtdq)'  # Remove orphaned packages
 
-#Other
-alias please='sudo $(fc -ln -1)'					# Re-run last command with sudo
-alias cputemp='sensors | grep Core'					# Show CPU temp
-alias mount='mount | column -t'						# Makes mount readable
-alias ip='curl icanhazip.com'						# Show current public IP
-alias ports='netstat -tulanp'						# Show open ports
-alias portscan='sudo nmap -sS -v -O'				# Scan for open TCP and UDP sockets on a remote address
+# Other
+alias please='sudo $(fc -ln -1)'                   # Re-run last command with sudo
+alias cputemp='sensors | grep Core'                # Show CPU temp
+alias mount='mount | column -t'                    # Makes mount readable
+alias ip='curl icanhazip.com'                      # Show current public IP
+alias ports='netstat -tulanp'                      # Show open ports
+alias portscan='sudo nmap -sS -v -O'               # Scan for open TCP and UDP sockets on a remote address
 
+# Dotfile tracking
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
